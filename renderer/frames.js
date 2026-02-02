374818e29640dcc4c15c71f64172aa5f00dd7051
dhymasriyanto <gojin003@gmail.com>
2026-02-02 20:57:03 +0700

    adding constant of frames, so the code will clean

---
+++ renderer/frames.js
@@ -0,0 +1,37 @@
+export const blinkFrames = [
+	'assets/eyes_1.png',
+	'assets/eyes_closed.png',
+	'assets/eyes_2.png',
+	'assets/eyes_4.png',
+]
+export const breathFrames = [
+	'assets/body_idle_2.png',
+	'assets/body_idle_3.png',
+	'assets/body_idle_4.png',
+	'assets/body_idle_5.png',
+	'assets/body_idle_4.png',
+	'assets/body_idle_3.png',
+	'assets/body_idle_2.png',
+	'assets/body_idle_1.png',
+]
+
+export const sleepTransitionFrames = [
+	'assets/body_sleep_transition_1.png',
+	'assets/body_sleep_transition_2.png',
+	'assets/body_sleep_transition_3.png',
+	'assets/body_sleep_transition_4.png',
+]
+
+export const eyesSleepTransitionFrames = [
+	'assets/eyes_3.png',
+	'assets/eyes_2.png',
+	'assets/eyes_1.png',
+	'assets/eyes_closed.png',
+]
+
+export const sleepSnoreFrames = [
+	'assets/sleep_snore_1.png',
+	'assets/sleep_snore_2.png',
+	'assets/sleep_snore_3.png',
+	'assets/sleep_snore_4.png',
+]
