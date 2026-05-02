c216ed0bb744c6b0a905485442b31a32502cabc5
Dhymas Julyan Riyanto <32106219+dhymasriyanto@users.noreply.github.com>
2026-05-02 22:54:32 +0700

    Expose API to move window via preload script
    Add moveWindow function to the exposed 'api' object in the preload
    script, allowing renderer process to send coordinates to move the window
    through IPC.

--- preload/index.js
+++ preload/index.js
@@ -6,3 +6,7 @@ contextBridge.exposeInMainWorld('versions', {
   electron: () => process.versions.electron,
   ping: () => ipcRenderer.invoke('ping'),
 })
+
+contextBridge.exposeInMainWorld('api', {
+  moveWindow: (x, y) => ipcRenderer.send('move-window', {x, y}) 
+})
